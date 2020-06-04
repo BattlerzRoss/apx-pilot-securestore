1ef4f68d9baeac932ef01c9bedaf6a8b4705581b
Ross <ross@battlerz.com>
2020-06-04 15:01:45 +1000

    Add project files.

---
+++ Models/CardModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardAccept.Models
+{
+    public class CardModel
+    {
+        public int ID { get; set; }
+        public string cardNumber { get; set; }
+        
+        public DateTime expiry { get; set; }
+        
+        public string cardName {get;set;}
+        
+        public string cvv { get; set; }
+
+
+    }
+}
